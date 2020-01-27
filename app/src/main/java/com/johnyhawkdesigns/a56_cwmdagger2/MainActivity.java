65e54d8b34203e6ad5c25964986785e83c26eefa
Ahsan <johnyhawkahsan@gmail.com>
2020-01-27 13:56:50 +0500

    Initial Commit- CodingWithMitch Android Dagger2 course.

---
+++ app/src/main/java/com/johnyhawkdesigns/a56_cwmdagger2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.johnyhawkdesigns.a56_cwmdagger2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// https://codingwithmitch.com/courses/dagger22-android/application-component/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
